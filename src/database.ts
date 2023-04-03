edb48031d48a237cf7f0c6c668940b8d6f890940
Daniel Moniz <dkmoniz@cetim.ms>
2023-04-03 12:46:44 +0100

    chore: Production config ☁️

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,17 @@
 import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
+const connectionPath =
+  env.DATABASE_CLIENT === 'sqlite'
+    ? {
+        filename: env.DATABASE_URL,
+      }
+    : env.DATABASE_URL
+
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection: connectionPath,
+
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
--- src/env/index.ts
+++ src/env/index.ts
@@ -13,8 +13,9 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'test', 'production'])
     .default('development'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
